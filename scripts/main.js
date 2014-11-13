3e577ade47ffd13afcaf184bc6d1078e92099fa9
Mike Zevitas <mzevitas@gmail.com>
2014-11-13 11:59:43 -0500

    layout styles

--- scripts/main.js
+++ scripts/main.js
@@ -37,6 +37,7 @@
     routes: {
 
       '' : 'home',
+      'profile' : 'profile',
       'add' : 'addPost',
       'login' : 'userLogin',
       'signup' : 'userSignUp',
@@ -47,7 +48,12 @@
 
     home: function (){
 
-      new App.Views.ListPost({ collection: App.posts});
+      new App.Views.PublicPost({ collection: App.posts});
+    },
+
+    profile: function (){
+
+      new App.Views.ProfileView({ collection: App.posts });
     },
 
     editPost: function (postID) {
@@ -87,7 +93,67 @@
 
 (function () {
 
-  App.Views.ListPost = Parse.View.extend ({
+  App.Views.PublicPost = Parse.View.extend ({
+    tagName: 'ul',
+    className: 'public_published',
+
+    events: {},
+
+    template: _.template($('#public-blog-posts').html()),
+
+  initialize: function (options){
+
+    this.options= options;
+
+    this.collection.off();
+    this.collection.on('sync', this.postQuery, this);
+
+    $('#blogPosts').html(this.$el);
+
+    this.postQuery();
+  },
+
+  postQuery: function () {
+    var self = this;
+
+    var all_post = new Parse.Query(App.Models.Blog);
+  
+    all_post.find({
+      success: function (results) {
+        self.collection = results;
+        self.render();
+      }
+    });
+
+  },
+
+  render: function () {
+    var self = this;
+
+    this.$el.empty();
+
+    var local_collection = this.collection;
+
+    _.each(local_collection, function (p) {
+      self.$el.append(self.template(p.toJSON()));
+    });
+
+    return this;
+
+  }
+
+
+
+
+
+
+  });
+
+}());
+
+(function () {
+
+  App.Views.ProfileView = Parse.View.extend ({
     tagName: 'ul',
     className: 'published',
 
@@ -156,6 +222,7 @@
 
         $('#blogPosts').html(this.$el);
 
+
         // $('#postContent').autosize();
       },
 
@@ -180,7 +247,7 @@
           p.save(null, {
             success: function () {
               App.posts.add(p);
-              App.router.navigate('', { trigger: true });
+              App.router.navigate('profile', { trigger: true });
             }
           });
 
